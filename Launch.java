cd50391519662d6cda0e09fdbc885afcfb271af6
chinmayi-m-n <1cg21cs023@cittumkur.org>
2024-03-03 15:59:28 +0530

    modified cloned file

--- Launch.java
+++ Launch.java
@@ -2,4 +2,10 @@ class Launch{
 public static void main(String[] args){
 System.out.println("welcome to git and github");
 }
+public static void fun(){
+int a=10;
+int b=30;
+int sum=a+b;
+System.out.println(sum);
+}
 }
